12d67a6b1013f626b9edff04db85795b84b12556
A Sneha <sneha04arul@gmail.com>
2026-01-09 11:04:05 +0530

    Create Contact.js

---
+++ src/pages/Contact.js
@@ -0,0 +1,41 @@
+function Contact() {
+  return (
+    <div className="page">
+      <h1>Contact Me</h1>
+
+      <p><strong>Name:</strong> Sneha A</p>
+      <p><strong>Email:</strong> sneha04arul@email.com</p>
+      <p><strong>Phone:</strong> +91 6382059635</p>
+      <p><strong>Location:</strong> India</p>
+
+      <h3>Social Links</h3>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/sneha-a-b0458329a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
+
+      <p>
+        <a
+          href="https://github.com/Sneha-04Arul"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+
+      <p>
+        <a href="/resume.pdf" download>
+          Download Resume
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
